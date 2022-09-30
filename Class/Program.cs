854a437baf0d05e913a986d6044f9e2c65f25c09
Nikita Poletaev <nikitapoletaev44941@mail.ru>
2022-09-30 17:42:16 +0300

    Добавьте файлы проекта.

---
+++ Class/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("1,2 задания\nВведите номер дня ");
+            DateTime dt = Convert.ToDateTime("01.01.1900");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num < 1 || num > 365)
+                Console.WriteLine("Столько дней не бывает!");
+            else
+            {
+                dt = dt.AddDays(num - 1);
+                Console.WriteLine(dt.ToString("d MMMM", CultureInfo.CreateSpecificCulture("ru-RU")));
+            }
+
+            Console.Write("3 задание\nВведите номер года ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите номер дня ");
+            int day = Convert.ToInt32(Console.ReadLine());
+            DateTime newdate = Convert.ToDateTime("01.01.1904");
+            if(year%4==0 && year%100!=0 || year%400==0)
+            {
+                if (day < 1 || day > 366)
+                    Console.WriteLine("Столько дней не бывает!");
+                else
+                {
+                    newdate = newdate.AddDays(day - 1);
+                    Console.WriteLine(newdate.ToString("d MMMM", CultureInfo.CreateSpecificCulture("ru-RU")));
+                }
+            }
+            else
+            {
+                if (day < 1 || day > 365)
+                    Console.WriteLine("Столько дней не бывает!");
+                else
+                {
+                    newdate = newdate.AddDays(day - 1);
+                    Console.WriteLine(newdate.ToString("d MMMM", CultureInfo.CreateSpecificCulture("ru-RU")));
+                }
+            }
+        }
+    }
+}
---
+++ Home/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //zad1();
+            //zad2();
+            //zad3();
+            //zad4();
+            //zad5();
+            //zad6();
+            //zad7();
+            //zad8();
+            //zad9();
+            //zad10();
+            //zad11();
+            static void zad1()
+            {
+                try
+                {
+                    double n = double.Parse(Console.ReadLine());
+                    double m = double.Parse(Console.ReadLine());
+                    double diam = double.Parse(Console.ReadLine());
+                    if(n >= diam+1 && m >= diam+1)
+                    {
+                        Console.WriteLine($"Вася сможет пролезть");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Вася не смодет пролезть в дырку. Она слишком узкая(");
+                    }
+                }
+                catch(FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            static void zad2()
+            {
+                Console.WriteLine("2 ZADANIE");
+                Console.Write("Введите число от 1 до 9 включительно ");
+                if(int.TryParse(Console.ReadLine(), out int res) && res <=9 && res >=1)
+                {
+                    for(int i = 1; i < 10; i++)
+                    {
+                        Console.WriteLine($"{res}*{i} = {res*i}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Неверный ввод.Читать научись!");
+                }
+            }
+            static void zad3()
+            {
+                Console.Write("3 Задание.\nВведите целое число ");
+                try
+                {
+                    int num = int.Parse(Console.ReadLine());
+                    if (num % 3 == 0 && num % 5 == 0)
+                        Console.WriteLine("БэнгБум");
+                    else if (num % 3 == 0 && num % 5 != 0)
+                        Console.WriteLine("Бэнг");
+                    else if (num % 3 != 0 && num % 5 == 0)
+                        Console.WriteLine("Бум");
+                    else
+                        Console.WriteLine("Мимо");
+                }
+                catch(FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("Ты всё сломал, поздравляю!(нет)");
+                }
+            }
+            static void zad4()
+            {
+                Console.Write("4 Задание.Введите строку ");
+                try
+                {
+                    string stroka = Console.ReadLine();
+                    bool flag = true;
+                    foreach (char c in stroka)
+                    {
+                        if (char.IsUpper(c) == false)
+                            flag = false;
+                    }
+                    Console.WriteLine(flag ? "True" : "False");
+                }
+                catch(FormatException ex)
+                {
+                    Console.WriteLine(ex);
+                }
+
+            }
+            static void zad5()
+            {
+                Console.Write("5Задание.\nВведите положительное целое число ");
+                try
+                {
+                    uint num = uint.Parse(Console.ReadLine());
+                    for(int i = 1; i <= num; i++)
+                    {
+                        Console.Write($"{i} овца...");
+                    }
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            
+            }
+            static void zad6()
+            {
+                int[] list = new int[] {1,2,4,5,6 };
+                double k = list.Sum() - list[list.Length-1];
+                Console.WriteLine(k/(list.Length-1));
+            }
+
+        }
+    }
+}
